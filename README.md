4635f7ad96e2f36839160275035e70a31216aa4a
Qing Li <qli@pop-os.localdomain>
2020-11-06 15:00:38 +0100

    added test files

--- README.md
+++ README.md
@@ -4,3 +4,13 @@
 
 ## get_next_line
 ***The aim of this project is to code a function that returns a line ending with a newline, read from a file descriptor.***
+
+This project also introduces the concept of static variables in C programming.
+
+## Technical considerations
+
+- Allowed functions: *malloc*, *free*, *write*
+- All heap allocated memory space must be properly freed when necessary
+
+## Example
+
---
+++ main.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: qli <qli@student.codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/06 14:48:19 by qli           #+#    #+#                 */
+/*   Updated: 2020/11/06 14:59:55 by qli           ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int		main(int argc, char **argv)
+{
+	int     fd;
+	char    *line;
+
+	if (argc != 2)
+    {
+        printf("Please run \"./get_next_line file_name\"\n");
+        return (0);
+    }    
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1)
+        return (0);
+	while (get_next_line(fd, &line) == 1)
+	{
+		printf("%s\n", line);
+        free(line);
+	}
+    free(line);
+    close(fd);
+	return (0);
+}
